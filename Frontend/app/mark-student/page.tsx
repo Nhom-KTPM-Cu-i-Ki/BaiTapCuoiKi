36b6e54356fab7a986a7ccb2ec3d4389e38d5274
dungngoo <ngodung06vn@gmail.com>
2024-05-26 08:28:15 +0700

    Final

--- Frontend/app/mark-student/page.tsx
+++ Frontend/app/mark-student/page.tsx
@@ -1,31 +1,8 @@
-'use client'
-import React, { useEffect, useState } from "react";
+import React from "react";
 import MarkClass from "@/components/MarkClass";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { get } from "@/lib/http";
-interface Student{
-  studentId: number,
-  userId: number,
-  name: string,
-  totalCredits: number
-}
 
 export default function MarkStudent() {
-  const student_id = 1
-  const [student,setStudent] = useState<Student>();
-
-  useEffect(() => {
-    const fetchCourses = async () => {
-      try {
-        const res = await get<Student>("/api/v1/academy/students/student_id/"+student_id);
-        const data = res.data;
-        setStudent(data)
-      } catch (error) {
-        console.error("Failed to fetch courses", error);
-      }
-    };
-    fetchCourses();
-  }, []);
   return (
     <div className="flex flex-col justify-between p-24">
       <div className="flex w-full" role="infomation-student">
@@ -38,9 +15,11 @@ export default function MarkStudent() {
 
         <div className="w-3/12 leading-8" role="information-2">
           <ul>
-            <li>Họ tên: {student?.name}</li>
-            <li>Mã số sinh viên: {student?.studentId}</li>
-            <li>Tổng số tín chỉ: {student?.totalCredits}</li>
+            <li>MSSV: 19491261</li>
+            <li>MSSV: 19491261</li>
+            <li>MSSV: 19491261</li>
+            <li>MSSV: 19491261</li>
+            <li>MSSV: 19491261</li>
           </ul>
         </div>
         <div className="flex items-center w-7/12 text-3xl ">
--- Frontend/components/MarkClass.tsx
+++ Frontend/components/MarkClass.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   Table,
   TableBody,
@@ -9,7 +9,6 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { get } from "@/lib/http";
 
 const invoices = [
   {
@@ -40,40 +39,8 @@ const invoices = [
     cuoiKi: "9",
   },
 ];
-interface Grade{
-  gradeId:number,
-  courseDto:CourseDto,
-  regular:number,
-  mid:number,
-  finalOfTerm:number,
-  total:number
-}
-
-interface CourseDto{
-  courseId: number,
-  courseName: string,
-  credits: number
-}
-
-
 
 export default function MarkClass() {
-  const [grades,setGrades] = useState<Grade[]>();
-
-  const id = 1;
-  const sid = 1;
-  useEffect(() => {
-    const fetchCourses = async () => {
-      try {
-        const res = await get<Grade[]>("/api/v1/academy/grades/student/"+id);
-        const data = res.data;
-        setGrades(data)
-      } catch (error) {
-        console.error("Failed to fetch courses", error);
-      }
-    };
-    fetchCourses();
-  }, []);
   return (
     <div className="pb-6">
       <div className="flex text-3xl justify-center pb-4 border-b-2 pt-6">
@@ -84,28 +51,28 @@ export default function MarkClass() {
           <TableHeader>
             <TableRow>
               <TableHead className="text-center">STT</TableHead>
+              <TableHead className="text-center">Mã LHP</TableHead>
               <TableHead>Tên lớp học phần</TableHead>
+              <TableHead className="text-center">Lớp</TableHead>
               <TableHead className="text-center">Thường kỳ 1</TableHead>
               <TableHead className="text-center">Thường kỳ 2</TableHead>
               <TableHead className="text-center">Cuối kỳ </TableHead>
-              <TableHead className="text-center">Tổng kết </TableHead>
-              <TableHead className="text-center">Xuất loại </TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {grades?.map((grade) => (
-              <TableRow key={grade.gradeId}>
-                <TableCell className="text-center">{grades.indexOf(grade)+1}</TableCell>
-                <TableCell>{grade.courseDto.courseName}</TableCell>
-                <TableCell className="text-center">{grade.regular}</TableCell>
-                <TableCell className="text-center">{grade.mid}</TableCell>
+            {invoices.map((invoice) => (
+              <TableRow key={invoice.maLHP}>
+                <TableCell className="text-center">{invoice.stt}</TableCell>
+                <TableCell className="text-center">{invoice.maLHP}</TableCell>
+                <TableCell>{invoice.tenLHP}</TableCell>
+                <TableCell className="text-center">{invoice.lop}</TableCell>
                 <TableCell className="text-center">
-                  {grade.finalOfTerm}
+                  {invoice.thuongKy1}
                 </TableCell>
                 <TableCell className="text-center">
-                  {grade.total}
+                  {invoice.thuongKy2}
                 </TableCell>
-                <TableCell className="text-center">{grade.total>9?"Xuất sắc":grade.total<9?"Giỏi":grade.total<8?"Khá":grade.total<7?"Trung bình":grade.total<5?"Yếu":"Kém"}</TableCell>
+                <TableCell className="text-center">{invoice.cuoiKi}</TableCell>
               </TableRow>
             ))}
           </TableBody>
